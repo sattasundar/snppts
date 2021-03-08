5d88f8f07ced90f962fc70325433ae5ac1be6865
Satta Sundar <62828724+sattasundar@users.noreply.github.com>
2021-03-09 00:28:48 +0530

    Create SattaSundar.cs

---
+++ src/Snppts/Authors/SattaSundar.cs
@@ -0,0 +1,16 @@
+using System;
+using Snppts.Infrastructure;
+
+namespace Snppts.Authors
+{
+    public class SattaSundar : IAmAnAuthor
+    {
+        public string FirstName => "Satta";
+        public string LastName => "Sundar";
+        public Uri Website => new Uri("https://github.com/sattasundar");
+        public string TwitterHandle => "Sattasundarp";
+        public string GitHubHandle => "sattasundar";
+        public string EmailAddress => "sundarsatta@outlook.com";
+        public string GravatarHash => "56d40a697b55b9813a5a6d356db0011d";
+    }
+}
